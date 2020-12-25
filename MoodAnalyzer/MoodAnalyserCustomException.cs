6561be32229f2bf4c2ff5834e089f237b998e681
Anuj Sharma <intrstelr.007@gmail.com>
2020-12-25 23:02:04 +0530

    [Anuj] Refactor. Added and made changes to xml documentation

--- MoodAnalyzer/MoodAnalyserCustomException.cs
+++ MoodAnalyzer/MoodAnalyserCustomException.cs
@@ -4,9 +4,16 @@ using System.Text;
 
 namespace MoodAnalyzer
 {
+    /// <summary>
+    /// Mood Analyzer Custom Exception class
+    /// </summary>
+    /// <seealso cref="System.Exception" />
     public class MoodAnalyserCustomException:Exception
     {
-        
+
+        /// <summary>
+        /// Enum Exception type
+        /// </summary>
         public enum ExceptionType 
         {
             ENTERED_NULL, ENTERED_EMPTY,
@@ -14,6 +21,11 @@ namespace MoodAnalyzer
             NO_SUCH_CLASS,OBJECT_CREATION_ISSUE
         }
         public ExceptionType type;
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MoodAnalyserCustomException"/> class.
+        /// </summary>
+        /// <param name="type">The type.</param>
+        /// <param name="message">The message.</param>
         public MoodAnalyserCustomException(ExceptionType type,string message):base(message)
         {
             this.type = type;
